e65d23968a3049e3ce5b9a500d306b1afe975e0d
Jason Sykes <jasonsykes@dreamfactory.com>
2014-05-08 09:23:13 -0400

    updated to handle sessions

--- dreamfactory.js
+++ dreamfactory.js
@@ -4,26 +4,28 @@ var BASE_PATH = "YOUR_DSP.com";
 var END_POINT = "/rest";
 //enter the app_name from the app you created to use with titanium
 var APP_KEY = "YOUR_APP_NAME";
-exports.makeRequest = function (method, path, body) {
+var SESSION_ID = "";
+exports.makeRequest = function(method, path, body) {
     var promise = Promise.defer();
     var url = BASE_PATH + END_POINT + path;
     var client = Titanium.Network.createHTTPClient();
-    client.onload = function () {
+    client.onload = function() {
         try {
             var data = JSON.parse(this.responseText);
             promise.resolve(data);
+
         } catch (e) {
             promise.reject(e);
         }
     };
-    client.onerror = function () {
+    client.onerror = function() {
         promise.reject(this);
     };
 
     client.open(method, url);
     client.setRequestHeader("X-DREAMFACTORY-APPLICATION-NAME", APP_KEY);
+    client.setRequestHeader("X-DREAMFACTORY-SESSION-TOKEN", SESSION_ID);
     client.send(body);
 
     return promise;
 };
-
